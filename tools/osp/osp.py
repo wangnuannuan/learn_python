57df04c93acb87999518b5f9254776d111b49dca
Jingru <jingru@synopsys.com>
2018-09-30 17:45:11 +0800

    cli
    Signed-off-by: Jingru <jingru@synopsys.com>

---
+++ tools/osp/osp.py
@@ -0,0 +1,186 @@
+from __future__ import print_function, division, absolute_import
+from tools.download_manager import generate_yaml, edit_yaml, getcwd
+from tools.notify import TerminalNotifier
+from tools.settings import *
+import yaml
+import os
+class OSP(object):
+    def __init__(self, file="osp.yaml"):
+        self.path = os.path.join(os.path.expanduser("~"), '.embarc_cli')
+        if not os.path.exists(self.path):
+            try:
+                os.mkdir(self.path)
+            except (IOError, OSError):
+                pass
+        self.file = file
+
+        fl = os.path.join(self.path, self.file)
+        if not os.path.exists(fl):
+            self.cfg_dict = dict()
+            self.cfg_dict["path"] = dict()
+            generate_yaml(fl, self.cfg_dict)
+        else:
+            self.cfg_dict = yaml.load(fl)
+        self.current = self.get_path()
+
+    def set_path(self, path ,url=None):
+        fl = os.path.join(self.path, self.file)
+        try:
+            with open(fl) as f:
+                self.cfg_dict = yaml.load(f)
+                for key, path_dict  in self.cfg_dict.items():
+                    for url_, path_ in path_dict.items():
+                        if path==path_ :
+                            return
+                path_num = len(self.cfg_dict)
+                path_key = "path" + str(path_num + 1)
+                self.cfg_dict[path_key] = dict()
+                self.cfg_dict[path_key]["git"] = url
+                self.cfg_dict[path_key]["local"] = path
+                self.current = path
+                edit_yaml(fl, self.cfg_dict)
+
+        except IOError:
+            raise IOError("Can not open file %s ." % fl)
+
+    def get_path(self):
+        fl = os.path.join(self.path, self.file)
+        try:
+            with open(fl) as f:
+                self.cfg_dict = yaml.load(f)
+                for path, path_dict  in self.cfg_dict.items():
+                    if path_dict.get("local", None):
+                        osp_root = path_dict["local"]
+                        if self.is_osp(osp_root):
+                            return osp_root
+                return None
+
+        except IOError:
+            raise IOError("Can not open file %s ." % fl)
+    def clear_path(self):
+        fl = os.path.join(self.path, self.file)
+        try:
+            with open(fl) as f:
+                self.cfg_dict = dict()
+                edit_yaml(fl, self.cfg_dict)
+
+        except IOError:
+            raise IOError("Can not open file %s ." % fl)
+
+    def list_path(self):
+        fl = os.path.join(self.path, self.file)
+        notifier = TerminalNotifier()
+        try:
+            with open(fl) as f:
+                self.cfg_dict = yaml.load(f)
+
+                table_head = ["osp"]
+                table_content = list()
+                for path, path_dict in self.cfg_dict.items():
+                    table_content.append([path])
+                    string_list = list()
+                    for (key, value) in path_dict.items():
+                        if value == self.current:
+                            value = notifier.COLORS['green'] + value + notifier.COLORS['default']
+                        string_list.append(" %s: %s" % (key, value))
+                    # string_list = [ " %s: %s" % (key, value) for (key, value) in path_dict.items()]
+                    string = "\n".join(string_list)
+                    table_content.append([string])
+
+                notifier.event["format"] = "table"
+                notifier.event["message"] = [table_head, table_content]
+
+                notifier.notify(notifier.event)
+
+        except IOError:
+            raise IOError("Can not open file %s ." % fl)
+
+    def is_osp(self, path):
+        for key, path_dict in self.cfg_dict.items():
+            if path == key:
+                if "local" in path_dict:
+                    path = path_dict["local"]
+        if os.path.exists(path) and os.path.isdir(path):
+            for files in OSP_DIRS:
+                files_path = os.path.join(path, files)
+                if os.path.exists(files_path) and os.path.isdir(files_path):
+                    pass
+                else:
+                    return False
+            self.set_path(path=path ,url=None)
+            return path
+        else:
+            return False
+
+    def support_board(self, root):
+        result = []
+        board_path = os.path.join(root, "board")
+        if os.path.exists(board_path):
+            for file in os.listdir(board_path):
+                if os.path.isdir(os.path.join(board_path, file)):
+                    result.append(file)
+        return result
+
+    def get_board_version(self, root, board, bd_version=None):
+        result = []
+        board_path = "board/" + board
+        ver_path = os.path.join(root, board_path)
+        if os.path.exists(ver_path):
+            bd_vers_dict = self._board_version_config(root, board, bd_version)
+            result.extend(bd_vers_dict.keys())
+        return result
+
+    def _board_version_config(self, root, board, bd_version=None):
+        board_path = os.path.join(root, "board", board)
+        bd_vers = dict()
+        if os.path.exists(board_path):
+            files = os.listdir(board_path)
+            if "configs" in files:
+                versions = os.listdir(os.path.join(board_path, "configs"))
+                for version in versions:
+                    version_path = os.path.join(board_path, "configs", version)
+                    if os.path.isdir(version_path):
+                        bd_vers[version] = version_path
+            else:
+                versions = os.listdir(board_path)
+                for version in versions:
+                    path = os.path.join(board_path, version)
+                    if os.path.isdir(path) and "configs" in os.listdir(path):
+                        version_path = os.path.join(board_path, version, "configs")
+                        bd_vers[version] = version_path
+        if bd_version is not None:
+            if bd_version in bd_vers:
+                bd_ver = {bd_version: bd_vers[bd_version]}
+                return bd_ver
+        return bd_vers
+
+    def get_tcfs(self, root, board, bd_version, cur_core=None):
+        result = []
+        board_version_path_dict = self._board_version_config(root, board, bd_version)
+        board_path = board_version_path_dict[bd_version]
+
+        if os.path.exists(board_path):
+            if cur_core:
+                cur_core_file = cur_core + ".tcf"
+                for root, dirs, files in os.walk(board_path, topdown=True):
+                    if cur_core_file in files:
+                        result.append(cur_core)
+
+            else:
+
+                for root, dirs, files in os.walk(board_path, topdown=True):
+
+                    for file in files:
+                        filename, filesuffix = os.path.splitext(file)
+                        if not filesuffix == ".tcf":
+                            continue
+                        result.append(filename)
+
+        return result
+
+    def get_makefile(self, app_path):
+        for makefile in MakefileNames:
+            makefile_path = os.path.join(app_path, makefile)
+            if os.path.exists(makefile_path) and os.path.isfile(makefile_path):
+                return makefile_path
+        return None
---
+++ tools/project.py
@@ -0,0 +1,286 @@
+from tools.utils import merge_recursive, uniqify, pquery
+from tools.exporter import Exporter
+import os
+import random
+from tools.settings import *
+import logging
+from tools.notify import TerminalNotifier
+
+logger = logging.getLogger("ide.project")
+
+class Ide:
+    def __init__(self, path=None):
+        #self.name = name
+        self.ide = {}
+        self.ide["common"] = {}
+        self.ide["common"] = self._get_project_file_template()
+        self.notifier = TerminalNotifier()
+        if path:
+            self.ide["common"]["path"] = path
+        else:
+            self.ide["common"]["path"] = os.getcwd().replace("\\", "/")
+        os.chdir(self.ide["common"]["path"])
+        self.notifier.event["format"] = "string"
+        self.notifier.event["type"] = "info"
+        self.notifier.event["message"] = "Start to generate ide "
+        self.notifier.notify(self.notifier.event)
+    def _get_project_file_template(self, name="Default"):
+        project_template = {
+            "name": name,
+            "path":"",
+            "folder": "",
+            "root": "",
+            "outdir": "${ProjDirPath}",
+        }
+        return project_template
+
+    def _get_cproject_template(self):
+
+        cproject_template = {
+            "core": {}, #//duqu
+            "includes": [],
+            "defines": [],
+            "build": "",
+            "clean": "",
+            "toolchain": "",
+        }
+        return cproject_template
+
+    def _get_build_template(self):
+        build_template = {
+            "BOARD": "",
+            "BD_VER": "",
+            "CUR_CORE": "",
+            "TOOLCHAIN": "",
+            "APPL": "",
+
+
+        }
+        return build_template
+
+    def _get_makefile_config(self, build_template=None):
+        self.notifier.event["message"] = "Read makefile and get configuration "
+        self.notifier.notify(self.notifier.event)
+        makefile = None
+        for file in MakefileNames:
+            if os.path.exists(file) and os.path.isfile(file):
+                makefile = file
+
+        if not makefile:
+            logger.error("makefile doesn't exist")
+            return None
+        with open(makefile) as f:
+            lines = f.read().splitlines()
+            for line in lines:
+                if line.startswith("APPL "):
+                    build_template["APPL"] = (line.split("=")[1]).strip()
+
+                if line.startswith("BOARD"):
+                    build_template["BOARD"] = (line.split("=")[1]).strip()
+                if line.startswith("BD_VER"):
+                    build_template["BD_VER"] = (line.split("=")[1]).strip()
+                if line.startswith("CUR_CORE"):
+                    build_template["CUR_CORE"] = (line.split("=")[1]).strip()
+                if line.startswith("TOOLCHAIN"):
+                    build_template["TOOLCHAIN"] = (line.split("=")[1]).strip()
+                    if build_template["TOOLCHAIN"] and build_template["TOOLCHAIN"] != "":
+                        pass
+                    else:
+                        build_template["TOOLCHAIN"] = "gnu"
+                if line.startswith("EMBARC_ROOT"):
+                    relative_root = (line.split("=")[1]).strip()
+
+                    osp_root = os.path.normpath(os.path.join(os.getcwd(), relative_root))
+                    self.ide["common"]["root"] = osp_root.replace("\\", "/")
+                    self.ide["common"]["folder"] = os.path.relpath(os.getcwd(), osp_root).replace("\\", "/").strip("../")
+
+        return build_template
+
+    def _get_project_conf_template(self):
+        cproject_template = self._get_cproject_template()
+        build_template = self._get_build_template()
+
+        make_tool = "make"
+        if build_template["TOOLCHAIN"] == "mw":
+            make_tool = "gmake"
+        opt_command = [make_tool]
+        opt_command.append("opt")
+        cmd_output = pquery(opt_command)
+
+        includes = list()
+        compile_opts = ""
+        relative_root = ""
+        if cmd_output:
+            opt_lines = cmd_output.splitlines()
+            for opt_line in opt_lines:
+                if opt_line.startswith("APPL"):
+                    build_template["APPL"] = (opt_line.split(":", 1)[1]).strip()
+                if opt_line.startswith("BOARD"):
+                    build_template["BOARD"] = (opt_line.split(":", 1)[1]).strip()
+                if opt_line.startswith("BD_VER"):
+                    build_template["BD_VER"] = (opt_line.split(":", 1)[1]).strip()
+                if opt_line.startswith("CUR_CORE"):
+                    build_template["CUR_CORE"] = (opt_line.split(":", 1)[1]).strip()
+                if opt_line.startswith("TOOLCHAIN"):
+                    build_template["TOOLCHAIN"] = (opt_line.split(":", 1)[1]).strip()
+                if opt_line.startswith("EMBARC_ROOT"):
+                    relative_root = (opt_line.split(":", 1)[1]).strip()
+                    osp_root = os.path.normpath(os.path.join(os.getcwd(), relative_root))
+                    self.ide['common']['osp_root'] = os.path.basename(osp_root)
+                if  opt_line.startswith("COMPILE_OPT") == True:
+                    compile_opt_line = opt_line.split(":", 1)[1]
+                    compile_opts = compile_opt_line.split()
+                    print(compile_opts)
+        self.notifier.event["message"] = "Get inculdes and defines "
+        self.notifier.notify(self.notifier.event)
+        if compile_opts != "" and relative_root != "":
+            for comp_opt in compile_opts:
+                if comp_opt.startswith("-I") == True:
+                    inc_path = comp_opt.replace("-I", "", 1) 
+                    if inc_path.startswith(relative_root):
+                        inc_path = os.path.relpath(inc_path, relative_root)
+                    inc_path = inc_path.replace("\\","/")
+                    print(inc_path)
+
+                    includes.append(inc_path)
+                if comp_opt.startswith("-D") == True:
+                    define = comp_opt.replace("-D", "", 1)
+
+                    define = define.replace('\\"', '&quot;')
+                    define = define.replace('"', '&quot;')
+
+                    cproject_template["defines"].append(define)
+
+
+        build_template = self. _get_makefile_config(build_template)
+        self.notifier.event["message"] = "Current configuration "
+        self.notifier.notify(self.notifier.event)
+        self.notifier.event["format"] = "table"
+        table_head = list()
+        table_content = list()
+        for key, value in build_template.items():
+            table_head.append(key)
+            table_content.append(value)
+        self.notifier.event["message"] = [table_head, [table_content]]
+        self.notifier.notify(self.notifier.event)
+        build_template["OUT_DIR_ROOT"] = "${ProjDirPath}"
+
+
+
+        cur_core = build_template["CUR_CORE"]
+        self.ide["common"]["name"] = build_template["APPL"]
+
+        for core, settings in CORES.items():
+            if cur_core == core:
+                cproject_template["core"] = {core: settings}
+
+        for core, settings in cproject_template["core"].items():
+            core_id = random.randint(1000000000, 2000000000)
+            cproject_template["core"][core]["id"] = core_id
+
+        for path in includes:
+            include = os.path.join(self.ide['common']["name"], path)
+            
+            if path == ".":
+                include = ""
+            include = include.replace("\\", "/")
+            cproject_template["includes"].append(include)
+        build_opt_list = ["%s=%s" % (key.upper(), value) for (key, value) in build_template.items()]
+        # cproject_template["build"] = " ".join(build_opt_list) + " all"
+        # cproject_template["clean"] = " ".join(build_opt_list) + " clean"
+        self.ide["toolchain"] = build_template["TOOLCHAIN"]
+        self.ide["common"]["links"] = includes
+        links_dict = dict()
+        for path in self.ide["common"]["links"]:
+            if path != "." and "embARC_generated" not in path:
+                if "/" not in path:
+                    links_dict[path] = [path]
+                else:
+                    path_depth = len(path.split("/"))
+                    path_list = [path.rsplit("/", i)[0] for i in range(path_depth, 0, -1)]
+                    path_list = uniqify(path_list)
+                    if path_list[0] not in links_dict:
+                        links_dict[path_list[0]] = path_list
+                    else:
+                        links_dict[path_list[0]].extend(path_list)
+                        links_dict[path_list[0]] = uniqify(links_dict[path_list[0]])
+        print(links_dict)
+        links_dict_sort = dict()
+
+        
+        for key ,value in links_dict.items():
+            length = len(value) - 1
+            links_dict_sort[value[length]] = list()
+            for i in range(length):
+                links_dict_sort[value[length]].append(value[i])
+        self.ide["common"]["links"] = links_dict_sort
+        print(self.ide["common"]["links"])
+
+
+
+        return cproject_template
+
+    @staticmethod
+    def _list_elim_none(list_to_clean):
+        return [l for l in list_to_clean if l]
+
+    @staticmethod
+    def _dict_elim_none(dic_to_clean):
+        dic = dic_to_clean
+        try:
+            for k, v in dic_to_clean.items():
+                if type(v) is list:
+                    dic[k] = Ide._list_elim_none(v)
+                elif type(v) is dict:
+                    dic[k] = Ide._dict_elim_none(v)
+        except AttributeError:
+            pass
+        return dic
+
+    def _set_project_attributes(self, key_values, destination, source): # source --> destination
+        if key_values in source:
+            for attribute, data in source[key_values].items():
+                if attribute in destination:
+
+                    if type(destination[attribute]) is list:
+                        if type(data) is list:
+                            destination[attribute].extend(data)
+                        else:
+                            destination[attribute].append(data)
+                    elif type(destination[attribute]) is dict:
+                        destination[attribute] = Ide._dict_elim_none(merge_recursive(destination[attribute], data))
+                    else:
+                        if type(data) is list:
+                            if data[0]:
+                                destination[attribute] = data[0]
+                        else:
+                            if data:
+                                destination[attribute] = data[0]
+    def get_asm_c_include(self):
+        self.ide["exporter"] = self._get_project_conf_template()
+        self.ide["exporter"].update(self.ide["common"])
+
+    def generate(self):
+
+        self.get_asm_c_include()
+        outdir = "."
+        if "path" in self.ide["common"]:
+            app_path = self.ide["common"]["folder"]
+            path_depth = len(app_path.split("/"))
+            #path_list = [app_path.rsplit("/", i)[0] for i in range(path_depth, 0, -1)]
+
+            # self.ide["common"]["path_list"] = uniqify(path_list)
+        print(self.ide["exporter"])
+        
+
+        exporter = Exporter("toolchain")
+        self.notifier.event["format"] = "string"
+        self.notifier.event["message"] = "Start to generate ide files .project and .cproject accroding to templates"
+        self.notifier.notify(self.notifier.event)
+        exporter.gen_file_jinja("project.tmpl", self.ide["common"], ".project", outdir)
+        '''
+        exporter.gen_file_jinja(".cproject.tmpl", self.ide["exporter"], ".cproject", outdir)
+        self.notifier.event["message"] = "Finish generate ide files and they are in " + os.path.abspath(outdir)
+        self.notifier.notify(self.notifier.event)
+        self.notifier.event["message"] = "Open ide - >File >Open Projects from File System >Paste " + os.path.abspath(outdir)
+        self.notifier.notify(self.notifier.event)'''
